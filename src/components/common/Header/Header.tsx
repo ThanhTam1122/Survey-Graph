b245f06a2d0bcf7b1061258a08c886fba7f867a8
kkyy0720 <kuriyuki0720@gmail.com>
2021-12-04 17:19:02 +0900

    feat: Headerコンポーネント作成(#4)

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,33 @@
+import { VFC } from 'react';
+import { css } from '@emotion/react';
+import { colors } from '@/styles/constants';
+
+const Header: VFC = () => {
+  return (
+    <header css={header}>
+      <div css={[container, headerLayout]}>
+        <h1>RESAS Graph App</h1>
+      </div>
+    </header>
+  );
+};
+
+const header = css`
+  width: 100%;
+  padding: 16px 0;
+  background-color: ${colors.headerBgColor};
+`;
+
+const container = css`
+  max-width: 1280px;
+  padding: 0 4%;
+  margin: 0 auto;
+`;
+
+const headerLayout = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Header;
---
+++ src/components/common/Header/index.ts
@@ -0,0 +1,3 @@
+import Header from '@/components/common/Header/Header';
+
+export { Header };
