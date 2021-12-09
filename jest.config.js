bd9fb773193cf2597c5592e9c3b564aace49404b
kkyy0720 <kuriyuki0720@gmail.com>
2021-12-09 16:32:49 +0900

    test: テストライブラリのセットアップ(#11)

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  verbose: true,
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
