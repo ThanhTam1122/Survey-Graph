fb0312ffe1533891d35b07e9fdf6b1cf9bf285c8
kkyy0720 <kuriyuki0720@gmail.com>
2024-02-24 15:14:09 +0900

    update: emotionのアプデと設定を最新化 11.7.0 -> 11.11.3(#16)
    設定最新化により不要になったパッケージも除去。

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    emotion: true,
+    reactRemoveProperties: true,
+  },
 };
+
+module.exports = nextConfig;
